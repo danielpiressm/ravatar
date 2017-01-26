fe2dfa2f8703e42ae980dbb1ae18806359dd2317
danielpiressm <danielpiressm@gmail.com>
2017-01-26 19:31:02 +0000

    Integrating Kinect tracking + Vive Tracking
    ..

---
+++ Assets/Scripts/Body Tracking/Head/HeadCameraController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+using System.Collections;
+
+/// <summary>
+/// This class is responsible for updating the caracter head 
+/// rotation according to the HMD (Head Mounted Display) rotation.
+/// </summary>
+public class HeadCameraController : MonoBehaviour 
+{
+	public Transform cameraParent;
+    public Vector3 threshold;
+    public Transform CarlHip;
+
+    void Start()
+    {
+        
+    }
+
+ 	void LateUpdate()
+	{
+
+
+        CarlHip.position = new Vector3(UnityEngine.VR.InputTracking.GetLocalPosition(UnityEngine.VR.VRNode.CenterEye).x,
+            CarlHip.position.y,
+            UnityEngine.VR.InputTracking.GetLocalPosition(UnityEngine.VR.VRNode.CenterEye).z);
+
+        //this.transform.position = cameraParent.transform.position;
+        this.transform.position = new Vector3((-UnityEngine.VR.InputTracking.GetLocalPosition(UnityEngine.VR.VRNode.CenterEye).x) + threshold.x,
+                 (-UnityEngine.VR.InputTracking.GetLocalPosition(UnityEngine.VR.VRNode.CenterEye).y) + threshold.y,
+                 (-UnityEngine.VR.InputTracking.GetLocalPosition(UnityEngine.VR.VRNode.CenterEye).z) + threshold.z) +
+                 cameraParent.transform.position;
+    }
+}
