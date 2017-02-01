86d4f09169d5dda96f05e56ff366084d368b5d00
danielpiressm <danielpiressm@gmail.com>
2017-02-01 12:33:11 +0000

    Adding "full" body log

--- Assets/TestTask.cs
+++ Assets/TestTask.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public enum Tasks { Task1, Task2, Task3, Completed };
 
+public enum BodyLog { head, hip, torso, rightHand, leftHand, rightFoot, leftFoot, rightShin, leftShin };
+
 public class TestTask : MonoBehaviour {
 
     public string collisionLogfileName = "collisionLog";
@@ -26,6 +28,20 @@ public class TestTask : MonoBehaviour {
 
     Camera _trackedObj;
 
+    public GameObject head;
+    public GameObject rightHand;
+    public GameObject leftHand;
+    public GameObject rightFoot;
+    public GameObject leftFoot;
+    public GameObject rightShin; //shin=canela
+    public GameObject leftShin;
+
+    private Vector3[] lastBodyPos;
+    private string[] bodyStr;
+    private string[] bodyStrPath;
+
+    public bool fullbodyLog = false;
+
     int currentTrigger = 0;
 
     float threshold = 0.05f;
@@ -39,7 +55,8 @@ public class TestTask : MonoBehaviour {
         //with avatars, change for torso tracking
         _trackedObj = Camera.main;
         lastPos = _trackedObj.transform.position;
-        InitializeReport(); 
+        InitializeReport();
+        InitializeFullbodyReport();
 	}
 	
 	// Update is called once per frame
@@ -70,7 +87,19 @@ public class TestTask : MonoBehaviour {
         
         System.IO.File.WriteAllText(collisionLogfileName + ".csv",collisionLogStr);
         System.IO.File.WriteAllText(logFileName + ".csv", logStr);
-        System.IO.File.WriteAllText(pathLogFileName + ".csv", pathStr);
+        if(fullbodyLog)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                System.IO.File.WriteAllText(bodyStrPath[i], bodyStr[i]);
+            }
+            
+        }
+        else
+        {
+            System.IO.File.WriteAllText(pathLogFileName + ".csv", pathStr);
+        }
+        
       
     }
 
@@ -79,6 +108,236 @@ public class TestTask : MonoBehaviour {
         float currentTime = Time.realtimeSinceStartup;
         logStr += currentTask.ToString() + "," + (currentTime - lastTimeBetweenTasks)+"\n";
         lastTimeBetweenTasks = currentTime;
+    }
+
+    void InitializeFullbodyReport()
+    {
+        lastBodyPos = new Vector3[9];
+        bodyStrPath = new string[9];
+        bodyStr = new string[9];
+        if(rightFoot!=null)
+        {
+            lastBodyPos[(int)BodyLog.rightFoot] = rightFoot.transform.position;
+            bodyStrPath[(int)BodyLog.rightFoot] = "rightFootLog.csv";
+            //create a file for each part of the body
+        }
+        if(leftFoot!=null)
+        {
+            //log this
+            lastBodyPos[(int)BodyLog.leftFoot] = leftFoot.transform.position;
+            bodyStrPath[(int)BodyLog.leftFoot] = "leftFootLog.csv";
+        }
+        if(rightHand!=null)
+        {
+            lastBodyPos[(int)BodyLog.rightHand] = rightHand.transform.position;
+            bodyStrPath[(int)BodyLog.rightHand] = "rightHandLog.csv";
+        }
+        if(leftHand!=null)
+        {
+            lastBodyPos[(int)BodyLog.leftHand] = leftHand.transform.position;
+            bodyStrPath[(int)BodyLog.leftHand] = "leftHandLog.csv";
+        }
+        if(rightShin!=null)
+        {
+            lastBodyPos[(int)BodyLog.rightShin] = rightShin.transform.position;
+            bodyStrPath[(int)BodyLog.rightShin] = "rightShinLog.csv";
+        }
+        if(leftShin!=null)
+        {
+            lastBodyPos[(int)BodyLog.leftShin] = leftShin.transform.position;
+            bodyStrPath[(int)BodyLog.leftShin] = "leftShinLog.csv";
+        }
+        if(head!=null)
+        {
+            lastBodyPos[(int)BodyLog.head] = head.transform.position;
+            bodyStrPath[(int)BodyLog.head] = "headLog.csv";
+        }
+        
+    }
+
+    void updateFullbodyReport()
+    {
+        Vector3 currentPosVector = new Vector3();
+        if (rightFoot != null)
+        {
+            currentPosVector = rightFoot.transform.position - lastBodyPos[(int)BodyLog.rightFoot];
+            if (currentPosVector.magnitude < threshold )
+            {
+                lastBodyPos[(int)BodyLog.rightFoot] = rightFoot.transform.position;
+                bodyStr[(int)BodyLog.rightFoot] += pathStr += string.Join(",", new string[]
+                {
+                    currentTask.ToString(),
+                    currentTrigger.ToString(),
+                    rightFoot.transform.position.x.ToString(),
+                    rightFoot.transform.position.y.ToString(),
+                    rightFoot.transform.position.z.ToString(),
+                    currentPosVector.x.ToString(),
+                    currentPosVector.y.ToString(),
+                    currentPosVector.z.ToString(),
+                    rightFoot.transform.eulerAngles.x.ToString(),
+                    rightFoot.transform.eulerAngles.y.ToString(),
+                    rightFoot.transform.eulerAngles.z.ToString(),
+                    rightFoot.transform.position.magnitude.ToString(),
+                    "\n"
+
+                });
+            }
+            //create a file for each part of the body
+        }
+        if (leftFoot != null)
+        {
+            currentPosVector = leftFoot.transform.position - lastBodyPos[(int)BodyLog.rightFoot];
+            if (currentPosVector.magnitude < threshold)
+            {
+                lastBodyPos[(int)BodyLog.leftFoot] = leftFoot.transform.position;
+                bodyStr[(int)BodyLog.leftFoot] += pathStr += string.Join(",", new string[]
+                {
+                    currentTask.ToString(),
+                    currentTrigger.ToString(),
+                    leftFoot.transform.position.x.ToString(),
+                    leftFoot.transform.position.y.ToString(),
+                    leftFoot.transform.position.z.ToString(),
+                    currentPosVector.x.ToString(),
+                    currentPosVector.y.ToString(),
+                    currentPosVector.z.ToString(),
+                    leftFoot.transform.eulerAngles.x.ToString(),
+                    leftFoot.transform.eulerAngles.y.ToString(),
+                    leftFoot.transform.eulerAngles.z.ToString(),
+                    leftFoot.transform.position.magnitude.ToString(),
+                    "\n"
+
+                });
+            }
+        }
+        if (rightHand != null)
+        {
+            currentPosVector = rightHand.transform.position - lastBodyPos[(int)BodyLog.rightHand];
+            if (currentPosVector.magnitude < threshold)
+            {
+                lastBodyPos[(int)BodyLog.rightHand] = rightHand.transform.position;
+                bodyStr[(int)BodyLog.rightHand] += pathStr += string.Join(",", new string[]
+                {
+                    currentTask.ToString(),
+                    currentTrigger.ToString(),
+                    rightHand.transform.position.x.ToString(),
+                    rightHand.transform.position.y.ToString(),
+                    rightHand.transform.position.z.ToString(),
+                    currentPosVector.x.ToString(),
+                    currentPosVector.y.ToString(),
+                    currentPosVector.z.ToString(),
+                    rightHand.transform.eulerAngles.x.ToString(),
+                    rightHand.transform.eulerAngles.y.ToString(),
+                    rightHand.transform.eulerAngles.z.ToString(),
+                    rightHand.transform.position.magnitude.ToString(),
+                    "\n"
+
+                });
+            }
+        }
+        if (leftHand != null)
+        {
+            lastBodyPos[(int)BodyLog.leftHand] = leftHand.transform.position;
+            currentPosVector = leftHand.transform.position - lastBodyPos[(int)BodyLog.leftHand];
+            if (currentPosVector.magnitude < threshold)
+            {
+                lastBodyPos[(int)BodyLog.leftHand] = rightHand.transform.position;
+                bodyStr[(int)BodyLog.leftHand] += pathStr += string.Join(",", new string[]
+                {
+                    currentTask.ToString(),
+                    currentTrigger.ToString(),
+                    leftHand.transform.position.x.ToString(),
+                    leftHand.transform.position.y.ToString(),
+                    leftHand.transform.position.z.ToString(),
+                    currentPosVector.x.ToString(),
+                    currentPosVector.y.ToString(),
+                    currentPosVector.z.ToString(),
+                    leftHand.transform.eulerAngles.x.ToString(),
+                    leftHand.transform.eulerAngles.y.ToString(),
+                    leftHand.transform.eulerAngles.z.ToString(),
+                    leftHand.transform.position.magnitude.ToString(),
+                    "\n"
+
+                });
+            }
+        }
+        if (rightShin != null)
+        {
+            lastBodyPos[(int)BodyLog.rightShin] = rightShin.transform.position;
+            currentPosVector = rightShin.transform.position - lastBodyPos[(int)BodyLog.rightShin];
+            if (currentPosVector.magnitude < threshold)
+            {
+                lastBodyPos[(int)BodyLog.rightShin] = rightShin.transform.position;
+                bodyStr[(int)BodyLog.rightShin] += pathStr += string.Join(",", new string[]
+                {
+                    currentTask.ToString(),
+                    currentTrigger.ToString(),
+                    rightShin.transform.position.x.ToString(),
+                    rightShin.transform.position.y.ToString(),
+                    rightShin.transform.position.z.ToString(),
+                    currentPosVector.x.ToString(),
+                    currentPosVector.y.ToString(),
+                    currentPosVector.z.ToString(),
+                    rightShin.transform.eulerAngles.x.ToString(),
+                    rightShin.transform.eulerAngles.y.ToString(),
+                    rightShin.transform.eulerAngles.z.ToString(),
+                    rightShin.transform.position.magnitude.ToString(),
+                    "\n"
+
+                });
+            }
+        }
+        if (leftShin != null)
+        {
+            lastBodyPos[(int)BodyLog.leftShin] = leftShin.transform.position;
+            currentPosVector = leftShin.transform.position - lastBodyPos[(int)BodyLog.leftShin];
+            if (currentPosVector.magnitude < threshold)
+            {
+                lastBodyPos[(int)BodyLog.leftShin] = leftShin.transform.position;
+                bodyStr[(int)BodyLog.leftShin] += pathStr += string.Join(",", new string[]
+                {
+                    currentTask.ToString(),
+                    currentTrigger.ToString(),
+                    leftShin.transform.position.x.ToString(),
+                    leftShin.transform.position.y.ToString(),
+                    leftShin.transform.position.z.ToString(),
+                    currentPosVector.x.ToString(),
+                    currentPosVector.y.ToString(),
+                    currentPosVector.z.ToString(),
+                    leftShin.transform.eulerAngles.x.ToString(),
+                    leftShin.transform.eulerAngles.y.ToString(),
+                    leftShin.transform.eulerAngles.z.ToString(),
+                    leftShin.transform.position.magnitude.ToString(),
+                    "\n"
+
+                });
+            }
+        }
+        if (head != null)
+        {
+            lastBodyPos[(int)BodyLog.head] = head.transform.position;
+            currentPosVector = head.transform.position - lastBodyPos[(int)BodyLog.head];
+            if (currentPosVector.magnitude < threshold)
+            {
+                lastBodyPos[(int)BodyLog.head] = head.transform.position;
+                bodyStr[(int)BodyLog.head] += pathStr += string.Join(",", new string[]
+                {
+                    currentTask.ToString(),
+                    currentTrigger.ToString(),
+                    head.transform.position.x.ToString(),
+                    head.transform.position.y.ToString(),
+                    head.transform.position.z.ToString(),
+                    currentPosVector.x.ToString(),
+                    currentPosVector.y.ToString(),
+                    currentPosVector.z.ToString(),
+                    head.transform.eulerAngles.x.ToString(),
+                    head.transform.eulerAngles.y.ToString(),
+                    head.transform.eulerAngles.z.ToString(),
+                    head.transform.position.magnitude.ToString(),
+                    "\n"
+
+                });
+            }
+        }
     }
 
     void UpdatePathReport()
